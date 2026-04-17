9d3469b99ca3fe4bb9b3f2a4258ef6d795ac1526
Arga-12 <argarill226@gmail.com>
2026-04-17 07:52:56 +0700

    Inventry

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>My Web Gw</title>
+</head>
+<body>
+    <h1 class="flex items-center justify-center min-h-screen text-blue-600 text-3xl font-bold">Greetings, Argandull!</h1>
+</body>
+</html>
